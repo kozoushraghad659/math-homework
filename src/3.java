2d7c17eb6db33a74fa4f6f1f40ff130a5d6198bd
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-03-07 14:04:28 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MathHomework {
+ public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  System.out.println("Enter your first number: ");
+  int num1 = input.nextInt();
+  System.out.println("Enter your second number: ");
+  int num2 = input.nextInt();
+  System.out.println("The sum of the two numbers is: " + (num1 + num2));
+ }
+}
