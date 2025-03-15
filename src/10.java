f21f96ae8e63dc99d83545a50b8b3253b790d62c
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-03-15 15:53:27 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MathHomework {
+  public static void main(String[] args) {
+    int a = (int)(Math.random() * 10);
+    int b = (int)(Math.random() * 10);
+    System.out.println("What is " + a + " x " + b + "?");
+  }
+}
