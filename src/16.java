a39a012a0e87cbb21117bffae9e6ba656ab628cd
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-03-21 06:48:15 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 3.14;
+        String c = "Hello World";
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of b is: " + b);
+        System.out.println("C++ code:");
+        c = "Hello Python!";
+    }
+}
