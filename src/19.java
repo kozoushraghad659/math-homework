0d5f6ba96bb5d38e4404ecb4f24af7c41dc25c6b
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-03-24 00:11:35 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of how to add two numbers in Java
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2; // The result will be 8
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
