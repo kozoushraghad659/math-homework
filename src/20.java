3bb39e635ec6fce3f7c80295ac45aea84d3c50de
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-03-29 18:39:40 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.0;
+        System.out.println("The result of " + a + "/" + b + " is: " + (double)a/b);
+    }
+}
