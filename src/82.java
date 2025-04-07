c3df603cce83e7dd7928890d27b6850829445f62
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:01:39 +0000

    Add 82.java

---
+++ src/82.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for a simple mathematical calculation
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double result = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
