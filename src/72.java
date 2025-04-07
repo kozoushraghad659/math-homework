871ae3e6a899a620a2b0d9288df24844bceff160
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:48:43 +0000

    Add 72.java

---
+++ src/72.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of calculating square root using Java's Math class
+        double number = 16.0;
+        double result = Math.sqrt(number);
+        System.out.println("The square root of " + number + " is: " + result);
+    }
+}
