123cc428b49d1c58b7055e8e6466927db9db4bf4
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:04:02 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java code to solve math problems or exercises
+        
+        double result = 5 + 3 * 2 - 1;
+        System.out.println("The result is: " + result);
+        
+        // You can add more examples here
+    }
+}
