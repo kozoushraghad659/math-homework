3f1c5ed0d1ece835efb9b635798982672e438691
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:32:14 +0000

    Add 61.java

---
+++ src/61.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a simple arithmetic problem
+        int result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result);
+        
+        // Another example, solving an equation: 2x + 4 = 10
+        double x = (double) 4 / 2; // Calculate the value of x using division
+        System.out.println("The solution for x in the equation 2x + 4 = 10 is: " + x);
+    }
+}
