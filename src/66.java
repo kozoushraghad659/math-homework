f4a8496f241e8277132fd4229fcb7aca61fd4128
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:38:48 +0000

    Add 66.java

---
+++ src/66.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of an arithmetic problem
+        double sum = 1 + 2;
+        System.out.println("The result is: " + sum);
+        
+        // Another example of a mathematical operation
+        int difference = 8 - 4;
+        System.out.println("The result is: " + difference);
+    }
+}
