bf2e2957b8c718dfeba0a74e78900cf73ea86703
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:21:07 +0000

    Add 53.java

---
+++ src/53.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        boolean z = true;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+        System.out.println("z is " + z);
+    }
+}
