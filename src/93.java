c743686d1ecdffd038e174e9c91f7c87a9cec853
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:15:25 +0000

    Add 93.java

---
+++ src/93.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Your code goes here
+
+        int result = someCalculation;
+        System.out.println("The result is: " + result);
+    }
+
+    private double someCalculation() {
+        return 5 * 3 + 7 - 2 / 4;
+    }
+}
