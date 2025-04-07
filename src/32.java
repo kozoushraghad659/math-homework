e244af38e3ffb6eb92c9f02bea49f0e04387696a
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 14:53:06 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 6;
+        
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
