ff22af0bbaec18be88d553a84f780e844ae4983c
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:45:12 +0000

    Add 70.java

---
+++ src/70.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculations and logic within this method
+        System.out.println("Hello, World!");
+        double result = 5 + 3;
+        int age = 20;
+        String name = "John";
+        System.out.println(age);
+        if (age > 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+    }
+}
