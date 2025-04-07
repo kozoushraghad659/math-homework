7ef3826cf156fc0e5d189db0a8dd16435710eb02
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 14:39:57 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double x = 5.0;
+        int y = 3;
+        String z = "Java is fun";
+        boolean a = true;
+        float b = 2.5f;
+
+        // Add your code here
+    }
+}
