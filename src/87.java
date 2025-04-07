87cbc51c40786955451188e0f3cd35feed2a2e89
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:07:56 +0000

    Add 87.java

---
+++ src/87.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of solving a simple math problem: x + 2 = 5
+        double x = 3;
+        double solution = 5 - x;
+
+        System.out.println("Solution: " + solution);
+    }
+}
