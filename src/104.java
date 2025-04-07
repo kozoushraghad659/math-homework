3756163ba83ac1755aefb136600d70aae998cd8e
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:30:08 +0000

    Add 104.java

---
+++ src/104.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of using mathematical operations in Java
+        int result = 5 + 3;
+        System.out.println("The result is: " + result);
+        
+        double squareRoot = Math.sqrt(25);
+        System.out.println("The square root of 25 is: " + squareRoot);
+    }
+}
