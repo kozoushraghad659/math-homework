7ad34e985861b634af4e3df39d0c7edd50be78f2
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:01:31 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        double result = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + result);
+    }
+}
