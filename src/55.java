b359c301dc489732ef4465196267d885fc3877ff
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:24:16 +0000

    Add 55.java

---
+++ src/55.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        // Example calculation: a + b
+        double sum = (double)(a + b);
+        
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
