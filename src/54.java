5cb4bd09ef76a0a0b7addda9694a9a4294b1d961
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:22:53 +0000

    Add 54.java

---
+++ src/54.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("This is a Java program that demonstrates basic arithmetic operations.");
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double sum = num1 + num2;
+        double product = num1 * num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
