e5f78417adc9f07faf0c53a7e0274657e2285dae
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:33:46 +0000

    Add 107.java

---
+++ src/107.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical operations and calculations in Java
+        int a = 5;
+        int b = 3;
+        int result = (a + b) * 2;
+
+        System.out.println("The result is: " + result);
+    }
+}
