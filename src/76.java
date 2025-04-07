dc17a8fabf78226e655357b5f79676ea2e837a59
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:53:49 +0000

    Add 76.java

---
+++ src/76.java
@@ -0,0 +1,6 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code to solve the math problem goes here
+        System.out.println("Hello, World!");
+    }
+}
