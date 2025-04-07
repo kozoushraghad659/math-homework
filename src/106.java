07322e325e6446643fdd0c793dcea97cb41b11bb
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:32:37 +0000

    Add 106.java

---
+++ src/106.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("Result: " + (20 - 5) * 3);
+        // Another example with variables
+        double a = 10.5;
+        double b = 4.8;
+        System.out.println("a + b: " + (a + b));
+    }
+}
