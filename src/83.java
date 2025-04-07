2a0997c8fdba4900db78c66ef0567130f6c36ff6
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:02:53 +0000

    Add 83.java

---
+++ src/83.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example: Addition problem
+        System.out.println("3 + 5 = " + (3 + 5));
+        
+        // Example: Subtraction problem
+        System.out.println("10 - 2 = " + (10 - 2));
+        
+        // Example: Multiplication problem
+        System.out.println("4 * 6 = " + (4 * 6));
+        
+        // Example: Division problem
+        System.out.println("8 / 2 = " + (8 / 2));
+    }
+}
