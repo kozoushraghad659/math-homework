a3d7385c0a4486d1ba480e4a638dd1fb2ab3be49
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:35:07 +0000

    Add 108.java

---
+++ src/108.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code to calculate the area of a rectangle
+        double length = 10.5;
+        double width = 4.2;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+        
+        // Code to find the sum of two numbers
+        int num1 = 7;
+        int num2 = 3;
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
