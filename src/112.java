122eb0f74a1ddad272894cf02e655a001dbbf7b2
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:40:50 +0000

    Add 112.java

---
+++ src/112.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.5;
+        
+        System.out.println("x + y: " + (x + y));
+        System.out.println("x - y: " + (x - y));
+        System.out.println("x * y: " + (x * y));
+        System.out.println("x / y: " + (x / y));
+    }
+}
