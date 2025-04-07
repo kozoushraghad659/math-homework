8089bb715a24c6105a420649e57e283bdfa0e16b
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:28:55 +0000

    Add 103.java

---
+++ src/103.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical operation calculation
+
+        double result = 5.0 + 3.2 * 4 - 1.0 / 2;
+        System.out.println("The result of the expression is: " + result);
+    }
+}
