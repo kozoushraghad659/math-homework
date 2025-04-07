f59bdd7e01a07f0ec87be83eed32d1ba6f7677fe
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:25:14 +0000

    Add 100.java

---
+++ src/100.java
@@ -0,0 +1,26 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        
+        // Addition operation
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        
+        // Subtraction operation
+        int difference = a - b;
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+        
+        // Multiplication operation
+        int product = a * b;
+        System.out.println("The product of " + a + " and " + b + " is: " + product);
+        
+        // Division operation
+        int divisionResult = a / b;
+        System.out.println("The result of " + a + " divided by " + b + " is: " + divisionResult);
+        
+        // Modulus operation
+        int modulusResult = a % b;
+        System.out.println("The remainder of " + a + " divided by " + b + " is: " + modulusResult);
+    }
+}
