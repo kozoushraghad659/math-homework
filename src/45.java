6a1b44b51835ba8aa63f2f467a42390d83244cb9
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:10:09 +0000

    Add 45.java

---
+++ src/45.java
@@ -0,0 +1,6 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code to be executed in the Math Homework project
+        System.out.println("Hello, World!");
+    }
+}
