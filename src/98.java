92c9ed82f2c0e9abf9905106a5940e99acd07751
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:22:44 +0000

    Add 98.java

---
+++ src/98.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to demonstrate basic arithmetic operations
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The result of " + num1 + " + " + num2 + " is: " + (num1 + num2));
+    }
+}
