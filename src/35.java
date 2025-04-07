ddee9869daf5a2145a4a013c784d167875cd95ff
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 14:56:50 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 2.0;
+
+        System.out.println("The result of " + x + " plus " + y + " is: " + (x + y));
+        System.out.println("The square root of " + x + " is: " + Math.sqrt(x));
+    }
+}
