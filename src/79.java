d29a7b844d227750008a35244e2763097c7c94aa
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:57:49 +0000

    Add 79.java

---
+++ src/79.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = 2.5;
+
+        // Perform some operations on x and y
+        System.out.println("The result is: " + (x / y));
+        
+        // Print the values of x and y
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of y: " + y);
+    }
+}
