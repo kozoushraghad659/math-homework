87a6a416a94170845c645d82c3665e39395b486c
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 14:49:09 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical operation
+        double result = 5 + 3;
+        System.out.println("The result is: " + result);
+    }
+}
