5a13ac1231ba2fb90b1068e64c7e6ba3a3f90239
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:56:15 +0000

    Add 78.java

---
+++ src/78.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculation: 2 + 3
+        double sum = 2.0 + 3.0;
+        System.out.println("The result is: " + sum);
+    }
+}
