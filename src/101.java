9585f1e62f31e264ea0636feaa2d138a2d42858a
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:26:27 +0000

    Add 101.java

---
+++ src/101.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 3.5;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
