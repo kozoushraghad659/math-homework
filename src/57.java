725241aaf10f7dd05585c09107a38ee17aaf46c0
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:27:02 +0000

    Add 57.java

---
+++ src/57.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        float c = 3.14;
+        String d = "Hello";
+        System.out.println(a + b);
+        System.out.println(c + d);
+    }
+}
