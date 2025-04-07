e8b8ac6667c2ddc2431a60dbe4fc3f102662a4fc
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:06:42 +0000

    Add 86.java

---
+++ src/86.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of calculating basic operations
+        System.out.println("Adding 10 and 5: " + (10 + 5));
+        System.out.println("Subtracting 3 from 7: " + (7 - 3));
+        System.out.println("Multiplying 4 by 2: " + (4 * 2));
+        System.out.println("Dividing 8 by 2: " + (8 / 2));
+    }
+}
