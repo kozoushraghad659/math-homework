3c0b1cb7e63e382af11a931f563953d8a55ddb55
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:12:48 +0000

    Add 91.java

---
+++ src/91.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to demonstrate basic mathematical operations
+        int a = 5;
+        int b = 3;
+        double c = 2.5;
+
+        System.out.println("The result of addition: " + (a + b));
+        System.out.println("The result of subtraction: " + (c - b));
+        System.out.println("The result of multiplication: " + (a * b));
+        System.out.println("The result of division: " + (a / b));
+
+        // You can add more code here for the test, such as sorting arrays, creating a function, etc.
+    }
+}
