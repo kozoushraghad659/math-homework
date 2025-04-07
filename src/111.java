aad58b2b49f59bc4bc37ab6b984e4b26537932f9
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:39:05 +0000

    Add 111.java

---
+++ src/111.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 2 * a + 3 * b;
+
+        System.out.println("The value of C is: " + c);
+    }
+}
