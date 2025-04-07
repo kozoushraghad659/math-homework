c3d9ca5c202439da8592a21e4c4bc9950c692935
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:52:34 +0000

    Add 75.java

---
+++ src/75.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a simple arithmetic operation using Java's Math library
+        int num1 = 5;
+        int num2 = 3;
+        double result = num1 + num2;
+        System.out.println("The sum is: " + result);
+    }
+}
