72e5b09f8fad8fe92e3ba7d4cb0faa2ac824dde1
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:36:18 +0000

    Add 64.java

---
+++ src/64.java
@@ -0,0 +1,14 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for practicing addition and subtraction
+
+        int num1 = 5;
+        int num2 = 3;
+
+        int sum = num1 + num2; // 8
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        int difference = num1 - num2; // 2
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
