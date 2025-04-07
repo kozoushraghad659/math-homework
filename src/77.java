e39eac03d09cdce2967f2bf11d53fa7dfa96870a
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:54:57 +0000

    Add 77.java

---
+++ src/77.java
@@ -0,0 +1,19 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int result = 3 + 5;
+        System.out.println("The result of 3 + 5 is: " + result);
+
+        // Example 2: Subtraction
+        int difference = 8 - 2;
+        System.out.println("The result of 8 - 2 is: " + difference);
+
+        // Example 3: Multiplication
+        int product = 4 * 6;
+        System.out.println("The result of 4 * 6 is: " + product);
+
+        // Example 4: Division
+        long divisionResult = 10 / 2;
+        System.out.println("The result of 10 / 2 is: " + divisionResult);
+    }
+}
