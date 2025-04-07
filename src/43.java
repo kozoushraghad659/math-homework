2d43da0d1fd901afc52650804103b5dd7b643128
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:07:51 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for basic arithmetic operations
+        int sum = 5 + 3;
+        int difference = 8 - 2;
+
+        System.out.println("The result of the addition is: " + sum);
+        System.out.println("The result of the subtraction is: " + difference);
+
+        // Another example with a loop
+        for (int i = 0; i < 5; i++) {
+            int currentNumber = i * 2;
+            System.out.println(currentNumber);
+        }
+    }
+}
