c058f28ef29d66429833ad6f8df103452f99faf5
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 14:51:57 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical calculation
+        double result = 5 + 3;
+        System.out.println("The sum is: " + result);
+    }
+}
