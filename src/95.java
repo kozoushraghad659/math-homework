010f4f4de44833be8dd066c919e1bf8d5af69279
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:17:42 +0000

    Add 95.java

---
+++ src/95.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Random number generation
+        int randomNumber = (int)(Math.random() * 100);
+        
+        // Output the random number to the console
+        System.out.println("The random number generated is: " + randomNumber);
+    }
+}
