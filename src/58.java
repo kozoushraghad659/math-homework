27145138942235196e576fcd594d8745116804b3
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:28:07 +0000

    Add 58.java

---
+++ src/58.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical operations and logic
+        System.out.println("Addition: " + (20 + 15));
+        System.out.println("Subtraction: " + (30 - 10));
+        System.out.println("Multiplication: " + (40 * 20));
+        System.out.println("Division: " + (60 / 20));
+    }
+}
