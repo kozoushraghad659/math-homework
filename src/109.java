90ca81c9a360e82d5391956af9e5051097ba6901
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 16:36:28 +0000

    Add 109.java

---
+++ src/109.java
@@ -0,0 +1,19 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code 1: Simple addition and subtraction
+        System.out.println("The result of 5 + 3 is " + (5 + 3));
+        
+        // Example code 2: Addition with a variable
+        int num1 = 4;
+        int num2 = 7;
+        System.out.println("The result of " + num1 + " + " + num2 + " is " + (num1 + num2));
+        
+        // Example code 3: Simple multiplication
+        int num = 9;
+        System.out.println("The result of " + num + " * 3 is " + (num * 3));
+        
+        // Example code 4: Division with a variable
+        double fraction = 5.0;
+        System.out.println("The result of " + fraction / 2 + " is " + (fraction / 2));
+    }
+}
