4d1a8f839fda9ea46f0262d3076c0e201425557f
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:40:14 +0000

    Add 67.java

---
+++ src/67.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("5 + 3 = " + (5 + 3));
+        System.out.println("10 - 2 = " + (10 - 2));
+        System.out.println("8 * 4 = " + (8 * 4));
+    }
+}
