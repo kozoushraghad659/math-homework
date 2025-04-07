06958a96378cd0a616fb2aebafb0cc63b75f2c34
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-07 15:51:27 +0000

    Add 74.java

---
+++ src/74.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 3.0;
+        int result;
+
+        result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
