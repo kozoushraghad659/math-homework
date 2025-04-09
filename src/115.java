e0c141662056f8f51456cc59649f43c95f1b9abe
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-09 12:52:32 +0000

    Add 115.java

---
+++ src/115.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MathHomework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
