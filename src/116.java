ee162068a003d0d9e443e7ed0bbc1890e9e3da24
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-11 11:46:31 +0000

    Add 116.java

---
+++ src/116.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of adding two numbers using Java's built-in arithmetic operations
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
