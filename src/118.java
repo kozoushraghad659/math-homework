5540dde41c2a9252d6df61ed6c4020abfae772db
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-24 04:58:06 +0000

    Add 118.java

---
+++ src/118.java
@@ -0,0 +1,6 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2}, {3, 4}};
+        System.out.println(matrix[0][0] + " / " + matrix[0][1] + " = " + (matrix[0][0]/matrix[0][1]));
+    }
+}
