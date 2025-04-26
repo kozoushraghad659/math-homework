904cb3fc0a46f3814dcdf646a02e55c6a7eb4ab4
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-04-26 08:00:38 +0000

    Add 120.java

---
+++ src/120.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        System.out.println("The result of addition is: " + (a + b));
+        System.out.println("The result of multiplication is: " + (a * b));
+        System.out.println("The result of subtraction is: " + (a - b));
+    }
+}
