01b5b6a58e201471f8b6d563c44c84f03c914597
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-05-17 03:59:29 +0000

    Add 122.java

---
+++ src/122.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of an arithmetic operation in Java
+
+        double number1 = 5;
+        double number2 = 3.5;
+        double result = number1 + number2;
+
+        System.out.println("The sum is: " + result);
+    }
+}
