27d5234c3e2be6f24c5850a4253d801491960437
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-05-18 19:22:31 +0000

    Add 123.java

---
+++ src/123.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of some mathematical calculations
+        System.out.println("The sum of 5 and 3 is: " + (5 + 3));
+        System.out.println("Five times two is: " + (5 * 2));
+        System.out.println("Ten divided by five equals: " + (10 / 5));
+    }
+}
