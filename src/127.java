dd1a3df1f12540cd99bad96a070a74be011f593d
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-05-23 06:57:37 +0000

    Add 127.java

---
+++ src/127.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter two numbers: ");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
