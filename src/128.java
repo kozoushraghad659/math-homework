78f2e7020d699381bd8fc7c49636193352d63ef1
kozoushraghad659 <kozoushraghad659@gmail.com>
2025-05-24 15:09:30 +0000

    Add 128.java

---
+++ src/128.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code to calculate and print the area of a rectangle
+        int width = 10;
+        int height = 5;
+        double area = width * height;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
